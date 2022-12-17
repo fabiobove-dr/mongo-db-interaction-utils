68b697e3f0fb6a330e89598f37772f79b5710c29
Fabio Bove <fabio.bove.dr@gmail.com>
2022-12-17 14:57:44 +0100

    Updated utils

--- src/MongoUtils.py
+++ src/MongoUtils.py
@@ -81,6 +81,32 @@ class MongoUtils:
         
         
 
+    def init_collection(db):
+        # Create a new collection in our db: "celestial_bodies"
+        try:
+            collections_list = db.list_collection_names()
+            if COLLECTION in collections_list:
+                status = "Collection already exists."
+                collection = db.get_collection(COLLECTION)
+            else:
+                collection = db.celestial_bodies
+                status = "Collection created successfully."
+        except Exception as e:
+            status = f"Something went wrong during collection creation: \n {e}"
+            collection = None
+        return status, collection
+
+
+    def init_documents(data, collection):
+        # Insert the document into our collection
+        if collection.count_documents({}) > 0:  # We remove the old documents
+            collection.delete_many({})
+        try:
+            [collection.insert_one(celestial_body) for celestial_body in data]
+            status = "Documents loaded successfully."
+        except Exception as e:
+            status = f"Something went wrong during document insertion: \n {e}"
+        return status
 
 
 
